adbf7abfe456d10f73acebe7bebd625d76af5117
Plorplepomp <denn2725@vandals.uidaho.edu>
2016-11-26 15:02:06 -0800

    lots of stuff again

---
+++ html/src/org/unicon/scott_dennis/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package org.unicon.scott_dennis.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.unicon.Unicon2DWindowTest;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Unicon2DWindowTest();
+        }
+}
---
+++ ios/src/org/unicon/scott_dennis/IOSLauncher.java
@@ -0,0 +1,22 @@
+package org.unicon.scott_dennis;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import org.unicon.Unicon2DWindowTest;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Unicon2DWindowTest(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
